34e4465ebad6a37e14ee7b974df8fe754938abae
unknown <lab3121@etecjk.local>
2025-09-16 22:10:28 -0300

    angular

--- src/app/sexto-componente/sexto-componente.ts
+++ src/app/sexto-componente/sexto-componente.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Pessoa } from '../modelo/Pessoa';
 
 @Component({
   selector: 'app-sexto-componente',
-  imports: [FormsModule, CommonModule, FormGroup, ReactiveFormsModule, FormControl],
+  standalone: true,
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './sexto-componente.html',
   styleUrl: './sexto-componente.css'
 })
@@ -17,4 +19,40 @@ formulario = new FormGroup({
     cidade: new FormControl('', [Validators.required, Validators.minLength(3)])
 
 });
+
+// Visibilidade dos botões 
+btnCadastrar: boolean = true; 
+
+//Vetor Pessoa 
+Vetor: Pessoa []
+
+//Função cadastrar 
+
+cadastrar (){
+  //Inclusão da função do vetor
+  this.Vetor.push(this.formulario.value as Pessoa); 
+  
+  //Limpeza dos inputs 
+  this.formulario.reset();
+
+  //Visualização no console 
+  console.table(this.Vetor);
+}
+
+//Armazenar indice da pessoa selecionada
+indice: number = -1; 
+
+selecionar(indice:number){
+
+  //Atribuir o indice da pessoa
+ this.indice = indice;
+  
+ //Atribuir os dados das pessoa no formulario
+ this.formulario.setValue ({
+  nome: this.Vetor [indice].nome, 
+  idade: this.Vetor [indice].idade,
+  cidade: this.Vetor [indice].cidade
+ });
+ this.btnCadastrar = false;
+}
 }
